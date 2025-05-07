ef8693350b33b778e074f34aec9a08f202272d74
zdrobix <zdroba88@gmail.com>
2025-05-07 18:47:33 +0300

    puled.

---
+++ PlataCuOra/PlataCuOra.Server/Models/Domain/User.cs
@@ -0,0 +1,26 @@
+﻿namespace PlataCuOra.Server.Models.Domain
+{
+	public class User
+	{
+		public int Id{ get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string Role { get; set; }
+
+		public User SetId (int id)
+		{
+			this.Id = id;
+			return this;
+		}
+
+		public User() { }
+		public User (string name, string email, string password, string role)
+		{
+			Name = name;
+			Email = email;
+			Password = password;
+			Role = role;
+		}
+	}
+}
