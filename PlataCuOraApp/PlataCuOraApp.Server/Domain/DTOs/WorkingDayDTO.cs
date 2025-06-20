4b90809790ce77693811393adec228fb42ab9065
iioana16 <138588681+iioana16@users.noreply.github.com>
2025-06-20 15:46:48 +0300

    Implementation for calendar

---
+++ PlataCuOraApp/PlataCuOraApp.Server/Domain/DTOs/WorkingDayDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace PlataCuOraApp.Server.Domain.DTOs
+{
+    public class WorkingDayDTO
+    {
+        public string Date { get; set; }
+        public string DayOfWeek { get; set; }
+        public bool IsWorkingDay { get; set; }
+        public string Parity { get; set; }
+    }
+
+}
